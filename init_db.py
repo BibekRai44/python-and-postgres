c58941bc66de39c683386c968bd3bf043804bec3
Bibekkun <bibekrai655@gmail.com>
2022-04-10 23:35:13 +0545

    changes in postgresql database

--- init_db.py
+++ init_db.py
@@ -4,8 +4,8 @@ import psycopg2
 conn=psycopg2.connect(
     host="localhost",
     database="flask_db",
-    user=os.environ['bibek'],
-    password=os.environ['broke']
+    user="bibek",
+    password="broke"
 )
 cur=conn.cursor()
 cur.execute('drop table if exists pl;')
@@ -13,14 +13,30 @@ cur.execute(''' create table pl(
                           id  int ,
                           name varchar(30) not null,
                           planguage varchar(20),
-                          info varchar(70))''')
-                                 
+                          info varchar(100))''')
+                               
 cur.execute('INSERT INTO pl (id,name, planguage, info)'
             'VALUES (%s,%s, %s, %s)',
-            (1,'sagar','python','powerful language in automation and ml'),
+           (1,'bibek','Python','powerful language in automation and ml')
+           )
+cur.execute('INSERT INTO pl (id,name, planguage, info)'
+            'VALUES (%s,%s, %s, %s)',
+           (2,'sagar','Java','Java is a high-level, class-based, object-oriented programming language')
+           )
+cur.execute('INSERT INTO pl (id,name, planguage, info)'
+            'VALUES (%s,%s, %s, %s)',
+           (3,'kishor','Go','Go is a statically typed, compiled programming language')
+           )
+cur.execute('INSERT INTO pl (id,name, planguage, info)'
+            'VALUES (%s,%s, %s, %s)',
+           (4,'asmit','Dart','Dart is a programming language designed for client development')
+           )
+cur.execute('INSERT INTO pl (id,name, planguage, info)'
+            'VALUES (%s,%s, %s, %s)',
+           (5,'nawaraj','Java','Java is a high-level, class-based, object-oriented programming language')
            )
 
 
 conn.commit()
 cur.close()
-conn.close()                            
+conn.close()                         
